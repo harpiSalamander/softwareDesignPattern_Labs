1104eb995b083e48611e712e5a7f34d9b4375efb
harpiSalamander <84271573+harpiSalamander@users.noreply.github.com>
2021-05-27 22:07:16 +0300

    Create Application.java

---
+++ Laboratory 3/src/main/java/Application/Application.java
@@ -0,0 +1,27 @@
+package Application;
+
+import Classes.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Application {
+
+	public static void main(String[] args) {
+		List<Thread> cashDesk = new ArrayList<>();
+		for (int i=0;i<4;i++) 
+			cashDesk.add(new Thread(new Generator(i+1)));
+		for (Thread thread : cashDesk) {
+			thread.start();
+		}
+		try {
+			Thread.sleep(300);
+		} catch (InterruptedException e) {}
+
+		for (Thread thread : cashDesk) {
+			thread.interrupt();
+		}
+	}
+
+}
