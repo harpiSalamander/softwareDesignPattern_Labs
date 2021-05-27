d2f5aa409903d16f9d46aa3a20973d3c12707afb
harpiSalamander <84271573+harpiSalamander@users.noreply.github.com>
2021-05-27 22:29:37 +0300

    Create Generator.java

---
+++ Laboratory 3/src/main/java/Classes/Generator.java
@@ -0,0 +1,38 @@
+package Classes;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Generator implements Runnable{
+	private int num;
+	private static int counter = 1;
+	
+	public Generator(int num) {
+		super();
+		this.num = num;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true) {
+			if (Thread.currentThread().isInterrupted()) break;
+			ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("CtxBuilder.xml");
+			Order order = ctx.getBean("Order", Order.class);
+			System.out.println("Касса " + num);
+			System.out.println("Заказ №" + counter + ":");
+			order.info();
+			System.out.println("Полная цена: " + order.getCost());
+			ctx.close();
+      counter +=1;
+      // Остановка потока, если достигли 10 заказа
+      if (counter > 10)
+			  break;	
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				break;
+			}
+		}
+		System.out.println("Касса " + num + " остановлена...");
+	}
+}
